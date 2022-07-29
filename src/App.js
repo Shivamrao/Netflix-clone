1234cb716c39158db41e1e76140c9f60b967c290
Shivamrao <shivamrao1902@gmail.com>
2022-07-29 13:11:36 +0530

    clone

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import requests from './requests';
 import './App.css';
+import Row from './Row';
+import Bannner from './Bannner';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Bannner className="banner1"/>
+     <div className="container1">
+     <Row title="Trending NOW "  fetchUrl={requests.fetchTrending} isRowLarge />
+     <Row title="Netflix Originals "  fetchUrl={requests.fetchNetflixOriginals}/>
+     <Row title="Top Rated"  fetchUrl={requests.fetchTopRated}/>
+     <Row title="Action Movies "  fetchUrl={requests.fetchActionMovies}/>
+     <Row title="Comedy Movies"  fetchUrl={requests.fetchComedyMovies}/>
+     <Row title="Horror Movies"  fetchUrl={requests.fetchHorrorMovies}/>
+     <Row title="Romance Movies"  fetchUrl={requests.fetchRomanceMovies}/>
+     </div>
     </div>
   );
 }
